6412d68a4acd97ca03e25da85ad30d8c99443d7e
Galin Malchev <g.malchev@live.com>
2022-11-23 20:08:44 +0200

    very minor change

--- src/components/MyLinks.js
+++ src/components/MyLinks.js
@@ -3,7 +3,7 @@ import React from "react";
 const MyLinks = () => {
   return (
     <div>
-      <h3>Please Visit My Website For a Pretier Look :D</h3>
+      <h3>Please Visit My Website For a Prettier Look :D</h3>
       <a href="https://gmalchev.com/">
       https://gmalchev.com/
       </a>
