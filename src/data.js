7bbe6da92bab466c085a651f08af2cb87dbeebf7
Galin Malchev <g.malchev@live.com>
2022-11-23 20:00:59 +0200

    changed the scaling of the images

--- src/data.js
+++ src/data.js
@@ -7,26 +7,26 @@ export const galleryItems = [
   {
     id: 2,
     imgLink:
-      "https://images.pexels.com/photos/13379805/pexels-photo-13379805.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "https://images.pexels.com/photos/237272/pexels-photo-237272.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
     id: 3,
     imgLink:
-      "https://images.pexels.com/photos/12880191/pexels-photo-12880191.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "https://images.pexels.com/photos/235922/pexels-photo-235922.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
     id: 4,
     imgLink:
-      "https://images.pexels.com/photos/13524648/pexels-photo-13524648.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "https://images.pexels.com/photos/2270328/pexels-photo-2270328.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
     id: 5,
     imgLink:
-      "https://images.pexels.com/photos/13524648/pexels-photo-13524648.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "https://images.pexels.com/photos/4974915/pexels-photo-4974915.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
     id: 6,
     imgLink:
-      "https://images.pexels.com/photos/13524648/pexels-photo-13524648.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "https://images.pexels.com/photos/4974912/pexels-photo-4974912.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
 ];
