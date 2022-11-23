6beeabe4d661ced3d643dc7c0251299a67343489
Galin Malchev <g.malchev@live.com>
2022-11-23 19:51:06 +0200

    very minor finishing things

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 
 import styles from "./About.module.css";
 
+import MyLinks from "./MyLinks";
+
 const About = () => {
   const [toggle, setToggle] = useState(false);
 
@@ -22,7 +24,7 @@ const About = () => {
       <button onClick={clickHandler} className={styles.toggle__btn}>
         Try Me
       </button>
-      {toggle && <p>There is more information showing here</p>}
+      {toggle && <MyLinks />}
     </div>
   );
 };
---
+++ src/components/MyLinks.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MyLinks = () => {
+  return (
+    <div>
+      <h3>Please Visit My Website For a Pretier Look :D</h3>
+      <a href="https://gmalchev.com/">
+      https://gmalchev.com/
+      </a>
+    </div>
+  );
+};
+
+export default MyLinks;
